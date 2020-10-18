83519ca7e4eb9458d36aae25de1f02b1670dc94b
WenYong123 <1028233948@qq.com>
2020-10-18 18:54:37 +0800

    这次一次测试

--- src/main/java/com/example/demo/TestConcer.java
+++ src/main/java/com/example/demo/TestConcer.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/testin")
 public class TestConcer {
     @GetMapping("/index")
     public String index(){
